3fba1482007b236dca4a53195402a827bb40f297
Annicha Jeerasawat <sirivimol.nine@gmail.com>
2021-10-28 10:37:50 +0700

    Complete ds-prep-example.

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "I would like to analyze the pictures of cancer types to learn/predict the disease and treat them",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "I would like to learn the basic of analysis and know more about how to make a data clean and neat"
+
+))
